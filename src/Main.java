28d5eec4929a209cc83b92f4023eeeff84d637e7
cami-cohete <esquivca@mail.uc.edu>
2022-06-07 11:30:40 -0400

    Final Commit. Finished Lab!

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,22 @@ public class Main {
 //    num WRIST_BAND_AGE = 21
 //    output “Enter your age: ”
 //    input userAge
-//      if userAge > = WRIST_BAND_AGE then
-//    output “You get a paper wristband!”
+//    if userAge > = WRIST_BAND_AGE then
+//       output “You get a paper wristband!”
 //    endIf
 
     public static void main(String[] args)
     {
-	// write your code here
+	int userAge = 0;
+    final int WRISTBAND_AGE = 21;
+
+    System.out.println("Enter your age: ");
+    userAge = 23;
+    System.out.println("Simulated user age is " + userAge);
+
+    if (userAge >= 21)
+    {
+        System.out.println("You get a wrist Band!");
+    }
     }
 }
