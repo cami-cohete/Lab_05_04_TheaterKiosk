b708971aaa8bac924a794faf051b4f29f7a5b008
cami-cohete <esquivca@mail.uc.edu>
2022-06-07 11:24:56 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+//    num userAge = 0
+//    num WRIST_BAND_AGE = 21
+//    output “Enter your age: ”
+//    input userAge
+//      if userAge > = WRIST_BAND_AGE then
+//    output “You get a paper wristband!”
+//    endIf
+
+    public static void main(String[] args)
+    {
+	// write your code here
+    }
+}
